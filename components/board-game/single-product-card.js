b2612e57af48a010d1eb95c4afdd16e6f2eab0d6
Po Cheng Lo <zxc45544@gmail.com>
2024-09-20 14:27:56 +0800

    側邊欄元件拆分，圖片路由變更，詳細頁面使用後端商品資料

--- components/board-game/single-product-card.js
+++ components/board-game/single-product-card.js
@@ -6,7 +6,15 @@ import { FiPlus } from 'react-icons/fi'
 import { FiMinus } from 'react-icons/fi'
 
 import Link from 'next/link'
-export default function SingleProductCard() {
+export default function SingleProductCard({
+  prodImg,
+  prodName,
+  prodIntro,
+  prodRules,
+  prodPeople,
+  prodTime,
+  prodPrice,
+}) {
   return (
     <>
       <div className="card mb-3">
@@ -14,7 +22,7 @@ export default function SingleProductCard() {
           {/* 商品圖片 */}
           <div className="col-12 col-xxl-6">
             <Image
-              src="https://i.postimg.cc/qq0yLkV7/099.jpg"
+              src={prodImg}
               className="img-fluid rounded-start"
               alt="..."
               width={700}
@@ -24,17 +32,13 @@ export default function SingleProductCard() {
           {/* 商品介紹 */}
           <div className="col-12 col-xxl-6">
             <div className="card-body">
-              <h2 className="card-title">小島洪水警報 Island Flood Alert</h2>
-              <p className="card-text">
-                遊戲介紹：「小島洪水警報」是一款充滿刺激與挑戰的桌遊。玩家將面對一場突如其來的洪水災害，需要在有限的時間內作出迅速的決策，保護島上的居民免受洪水的侵襲。遊戲中，玩家需協作並運用策略，找到安全的避難所，避免洪水淹沒整個小島。這款遊戲充滿危機感和刺激，適合喜歡挑戰和團隊合作的玩家。
-              </p>
-              <p className="card-text">
-                遊戲規則：玩家需協作應對洪水，利用資源找到避難所，最先保護所有居民者獲勝。
-              </p>
-              <p className="card-text">遊戲人數：2-6人</p>
-              <p className="card-text">遊戲時間：30-45分鐘</p>
+              <h2 className="card-title">{prodName}</h2>
+              <p className="card-text">{prodIntro}</p>
+              <p className="card-text">{prodRules}</p>
+              <p className="card-text">{prodPeople}</p>
+              <p className="card-text">{prodTime}</p>
               <div className="d-flex justify-content-between">
-                <h3 className="card-title">NT$1,750</h3>
+                <h3 className="card-title">{prodPrice}</h3>
                 <div className="d-flex">
                   <p className="card-text">數量</p>
                   <FiMinus />
