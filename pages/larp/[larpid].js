25f54f1b028a15ef24fd3ec469b183b28d087eb0
Zizi <rongnong2324@gmail.com>
2024-10-05 01:59:22 +0800

    切手機板

--- pages/larp/[larpid].js
+++ pages/larp/[larpid].js
@@ -82,7 +82,7 @@ export default function LarpId() {
   return (
     <div className={styles.larpBody}>
       {/* 置頂大圖 */}
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="position-relative">
         <h1
           className={`position-absolute ${styles.larpName} strokeText`}
@@ -91,22 +91,28 @@ export default function LarpId() {
           {escape.larp_name}
         </h1>
         <Image src={escape.larp_top_img} alt="" className="w-100" />
+        {/* 立即預約按鈕 */}
+        <Link
+          href="#order"
+          scroll={true}
+          className={styles.orderButton}
+          style={{ zIndex: '1' }}
+        >
+          {/* <OrderRightNow /> */}
+          <Image
+            // width={'auto'}
+            // height={'45px'}
+            className={styles.svgImg}
+            src="../larp/img/svgButton.png"
+            alt=""
+          />
+        </Link>
       </div>
-      {/* 立即預約按鈕 */}
-      <Link
-        href="#order"
-        scroll={true}
-        className={styles.orderButton}
-        style={{ zIndex: '1' }}
-      >
-        <OrderRightNow />
-      </Link>
-      {/* </div> */}
       <div className={styles.larpContainer} id={styles.larpStory}>
         {/* 故事背景分隔線 */}
         <Line title="故事背景" />
         {/* 故事內容 */}
-        <h4>{escape.larp_intro}</h4>
+        <h4 className={styles.h4Text}>{escape.larp_intro}</h4>
       </div>
       {/* 第一段大圖 */}
       <div id={styles.p1} className="position-relative">
@@ -135,8 +141,9 @@ export default function LarpId() {
           {/* 人數 */}
           <div className="d-inline flex-column text-white text-center">
             <svg
-              width={100}
-              height={99}
+              // width={100}
+              // height={99}
+              className={styles.svgBtn}
               viewBox="0 0 100 99"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -293,7 +300,6 @@ export default function LarpId() {
         <div
           id={styles.orderStep}
           className="d-flex justify-content-around align-items-center"
-          style={{ margin: '60px 189px 0 189px' }}
         >
           <div
             className="d-flex align-items-center"
