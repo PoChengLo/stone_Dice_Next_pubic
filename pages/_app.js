538b454f511314acf3d99480bcbf1c6c29f9b4ae
Po Cheng Lo <zxc45544@gmail.com>
2024-10-11 09:20:57 +0800

    修改首頁

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/style.css'
 import { CartProvider } from '@/hooks/use-cart-state'
 import { AuthProvider } from '@/hooks/use-auth'
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 
 export default function MyApp({ Component, pageProps }) {
   // 使用自訂在頁面層級的版面(layout)
   const getLayout = Component.getLayout || ((page) => page)
 
-  useEffect(() => {
-    // 確保 Tailwind 在所有頁面應用
-    import('tailwindcss/tailwind.css')
-  }, [])
+  // useEffect(() => {
+  //   // 確保 Tailwind 在所有頁面應用
+  //   import('tailwindcss/tailwind.css')
+  // }, [])
 
   return (
     <AuthProvider>
